7568be1b204099173cdde243b301dc041cdcfb9b
sunzhuhuan@jiangduoduo.com <sunzhuhuan@jiangduoduo.com>
2021-05-24 14:21:37 +0800

    tewts

--- go.mod
+++ go.mod
@@ -4,9 +4,9 @@ go 1.16
 
 require (
 	github.com/go-log/log v0.2.0
-	github.com/hashicorp/yamux v0.0.0-20210316155119-a95892c5f864 // indirect
 	github.com/klauspost/reedsolomon v1.9.11 // indirect
 	github.com/miekg/dns v1.1.40
+	github.com/panjf2000/ants v1.3.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/templexxx/cpufeat v0.0.0-20180724012125-cef66df7f161 // indirect
 	github.com/templexxx/xor v0.0.0-20191217153810-f85b25db303b // indirect
@@ -14,6 +14,7 @@ require (
 	github.com/valyala/fasthttp v1.25.0 // indirect
 	github.com/xtaci/kcp-go v5.4.20+incompatible
 	github.com/xtaci/lossyconn v0.0.0-20200209145036-adba10fffc37 // indirect
+	github.com/xtaci/smux v1.5.15 // indirect
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
 	golang.org/x/net v0.0.0-20210510120150-4163338589ed
 	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015
--- mux-c/Client.go
+++ mux-c/Client.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"crypto/rc4"
 	"fmt"
-	"github.com/hashicorp/yamux"
+	"github.com/xtaci/smux"
 	"io"
 	"log"
 	"net"
 	"sync"
-	"time"
 )
 
 const (
@@ -20,12 +20,15 @@ const (
 	twitter = "twitter"
 	mbga    = "mbga"
 )
+var pwd = "helloworld"
 
+type Rc4 struct {
+	C *rc4.Cipher
+}
 var (
 	smallBufferSize  = 4 * 1024  // 2KB small buffer
 	mediumBufferSize = 8 * 1024  // 8KB medium buffer
 	largeBufferSize  = 64 * 1024 // 32KB large buffer
-	queue1           = make(chan net.Conn, 1)
 )
 var (
 	sPool = sync.Pool{
@@ -56,180 +59,56 @@ func getPoolBig() []byte {
 	defer lPool.Put(b)
 	return b
 }
-func encryptCopy1(dst *net.TCPConn, src *net.TCPConn) {
-	defer dst.Close()
-	defer src.Close()
-	buf := make([]byte, 4096)
-	var err error
-	n := 0
-	for n, err = src.Read(buf); err == nil && n > 0; n, err = src.Read(buf) {
-		//5秒无数据传输就断掉连接
-		dst.SetDeadline(time.Now().Add(time.Second * 5))
-		src.SetDeadline(time.Now().Add(time.Second * 5))
-		dst.Write(buf[:n])
-	}
 
-}
-
-func encryptCopy2(dst *net.TCPConn, src *net.TCPConn) {
-	defer dst.Close()
-	defer src.Close()
-	buf := make([]byte, 4096)
-	var err error
-	n := 0
-	for n, err = src.Read(buf); err == nil && n > 0; n, err = src.Read(buf) {
-		//5秒无数据传输就断掉连接
-		dst.SetDeadline(time.Now().Add(time.Second * 5))
-		src.SetDeadline(time.Now().Add(time.Second * 5))
-		dst.Write(buf[:n])
-	}
-
-}
 
 func encryptCopy3(dst io.ReadWriter, src io.ReadWriter) {
-	buf := make([]byte, 4096)
+	buf := getPoolSmall()
 	var err error
 	n := 0
 	for n, err = src.Read(buf); err == nil && n > 0; n, err = src.Read(buf) {
-		//5秒无数据传输就断掉连接
+		buf = saltByte(buf)
 		dst.Write(buf[:n])
 	}
 
 }
 
-func handleClientRequest3(client *net.TCPConn) {
-	tcpaddr, err := net.ResolveTCPAddr("tcp4", "localhost:19077")
+func handleClientRequest3(client *net.TCPConn){
+
+	tcpaddr, err := net.ResolveTCPAddr("tcp4", "localhost:19078")
 	if err != nil {
 		log.Println("tcp地址错误", "address", err)
 		return
 	}
 	server, err := net.DialTCP("tcp", nil, tcpaddr)
-	session, err := yamux.Client(server, nil)
+	session, err := smux.Client(server, nil)
 
 	stream, err := session.Open()
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	//b2 := saltByte(b)
-	//server.Write([]byte(flagN))
 	//go encryptCopy1(client, server) //代理服务端发过来的是密文，编码后就成了明文，并传给浏览器
 	//go encryptCopy2(server, client) //客户端收到的是明文，编码后就成了密文并传给代理的服务端
 	go encryptCopy3(client, stream)
 	go encryptCopy3(stream, client)
-	/*if client == nil {
-		return
-	}
-	defer client.Close()
 
-	b := getPoolSmall()
-	n, err := client.Read(b[:])
 
-	var method, version, address string
-
-	num := bytes.IndexByte(b[:], '\r')
-	if num == -1 {
-		return
-	}
-	s := string(b[:num])
-	fmt.Sscanf(s, "%s%s%s", &method,&address,&version)
-	if(!strings.HasPrefix(address,"http://")){
-		address = "http://" + address
-	}
-	hostPortURL, err := url.Parse(address)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	if hostPortURL.Opaque == sslPort {
-		address = hostPortURL.Scheme + sslPort
-		if strings.Contains(address,"twitter") || strings.Contains(address,"twimg") {
-			server, err := net.Dial("tcp", "162.14.8.228:19077")
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			b2 := saltByte(b)
-			server.Write([]byte(flag))
-			server.Write(b2[:n])
-			//transport(server, client)
-			transport(client, server)
-		}else{
-			server, err := net.Dial("tcp", hostPortURL.Scheme + ":" + hostPortURL.Opaque)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-			if method == "CONNECT" {
-				fmt.Fprint(client, "HTTP/1.1 200 Connection established\r\n\r\n")
-			}
-			transport(client, server)
-		}
-	} else {
-		if strings.Index(hostPortURL.Host, ":") == -1 {
-			address = hostPortURL.Host + ":80"
-		} else {
-			address = hostPortURL.Host
-		}
-		tcpaddr, err := net.ResolveTCPAddr("tcp4", "localhost:19077")
-		if err != nil {
-			log.Println("tcp地址错误", address, err)
-			return
-		}
-		server, err := net.DialTCP("tcp", nil,tcpaddr)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		//b2 := saltByte(b)
-		//server.Write([]byte(flagN))
-		go encryptCopy(client, server) //代理服务端发过来的是密文，编码后就成了明文，并传给浏览器
-		go encryptCopy(server, client) //客户端收到的是明文，编码后就成了密文并传给代理的服务端
-	}*/
 }
 
 func saltByte(b []byte) []byte {
-	b2 := sPool.Get().([]byte)
-	defer sPool.Put(b2)
 	for k, v := range b {
 		if v == 0 {
-			b2[k] = 0
+			b[k] = 0
 		}
-		b2[k] = v ^ 2
+		b[k] = v ^ 2
 	}
-	return b2
+	return b
 }
 
-func transport(rw1, rw2 io.ReadWriter) error {
-	errc := make(chan error, 1)
-	go func() {
-		errc <- copyBuffer(rw1, rw2)
-	}()
-
-	go func() {
-		errc <- copyBuffer(rw2, rw1)
-	}()
-
-	err := <-errc
-	if err != nil && err == io.EOF {
-		err = nil
-	}
-	return err
-}
-
-func copyBuffer(dst io.Writer, src io.Reader) error {
-	buf := lPool.Get().([]byte)
-	defer lPool.Put(buf)
-
-	_, err := io.CopyBuffer(dst, src, buf)
-	return err
-}
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	tcpaddr, err := net.ResolveTCPAddr("tcp4", ":9077")
+	tcpaddr, err := net.ResolveTCPAddr("tcp4", ":9078")
 	if err != nil {
 		fmt.Println("侦听地址错", err)
 		return
@@ -243,6 +122,6 @@ func main() {
 		if err != nil {
 			log.Panic(err)
 		}
-		go handleClientRequest3(client)
+		handleClientRequest3(client)
 	}
 }
